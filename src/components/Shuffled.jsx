8d8d3ff4db5dfa5a268b8caca25859dab3af3d73
tomazcx <tomazcx06@gmail.com>
2022-06-22 21:58:54 -0300

    Reestructured page, remade generate password function

--- src/components/Shuffled.jsx
+++ src/components/Shuffled.jsx
@@ -2,71 +2,94 @@ import { useState } from "react"
 
 export const Shuffled = () => {
     const [charNum, setCharNum] = useState(0)
-    const [disableInput, setInput] = useState(true)
-    const [chars, setChars] = useState()
-    const [repeatCharacters, setRepeat] = useState(false)
-    const [finalPassword, setPassword] = useState()
+    const [checkedOne, setCheckedOne] = useState(false)
+    const [checkedTwo, setCheckedTwo] = useState(false)
+    const [checkedThree, setCheckedThree] = useState(false)
+    const [checkedFour, setCheckedFour] = useState(false)
+    const [finalPassword, setPassword] = useState("")
+    const [error, setError] = useState("")
 
-    const verifyInput = (num) =>{
-        if(num < 6){
-            setInput(true)
-        }else{
-            setInput(false)
-        }
-        setCharNum(num)
-    }
-    
-    const generatePassword = () =>{
-        const array = Array.from(chars)
-        let password = "";
-        for(let i = 0; i < array.length; i++){
-         password += randomChar(array)  
-        }
-        return password
+    const randomChar = (array) => {
+       
     }
 
-    const randomChar = (array) =>{
-        const num = Math.floor(Math.random()*array.length);
-        const char = array[num];
-        if(repeatCharacters){
-            if(char !== ""){
-                array[num] = ""
-                return char   
+
+    const generatePassword = () => {
+        try {
+            let characters = ""
+            let password = ""
+
+            if (checkedOne) {
+                characters += "abcdefghijklmnopqrstuvwxyz"
             }
-            return randomChar(array)
+
+            if (checkedTwo) {
+                characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            }
+
+            if (checkedThree) {
+                characters += "!#$%&+=-/@"
+            }
+
+            if (checkedFour) {
+                characters += "0123456789"
+            }
+
+            if(characters === "" || charNum < 6){
+                throw new Error
+            }
+
+
+            const array = Array.from(characters)
+            for (let i = 0; i < charNum; i++) {
+                const num = Math.floor(Math.random() * array.length)
+                password += array[num]
+            }
+
+            console.log(password)
+            setError("")
+            return password
+        } catch (e) {
+            setError("Invalid options.")
         }
 
-        return char
     }
 
     return (
-    <>
-        <div className='flex flex-col w-1/4 mb-4'>
-            <label htmlFor="number" className='text-white mb-4'>Number of characters (minimun of 6)</label>
-            <input type="number" id="number" onChange={(e) => verifyInput(e.target.value)} />
-        </div>
-
-        <div className='flex flex-col w-1/4 my-4'>
-            <label htmlFor="chars" className='text-white mb-4'>Characters to be shuffled (Up to {charNum})</label>
-            <input type="text" id="chars" maxLength={charNum} disabled={disableInput} onChange={(e) => setChars(e.target.value)}/>
-        </div>
-
-        <div className='flex flex-col w-1/4 my-4 text-white'>
-            <label htmlFor="repeat" >Repete characters?</label>
-            <div>
-                <input type="radio" name="repeat" defaultChecked id="0" onChange={event => setRepeat(false)}/>
-                <label htmlFor="0" className="ml-4 mr-8">No</label>
-                <input type="radio" name="repeat" id="1" onChange={event => setRepeat(true)}/>
-                <label htmlFor="1" className="ml-4">Yes</label>
+        <>
+            <div className='flex flex-col w-1/4 mb-4'>
+                <label htmlFor="number" className='text-white mb-4'>Number of characters (minimun of 6)</label>
+                <input type="number" id="number" onChange={(e) => setCharNum(e.target.value)} />
             </div>
-        </div>
 
-        <button className="mt-4" onClick={() => setPassword(generatePassword())}>Generate</button>
+            <div className='flex flex-col w-1/4 my-4 text-white'>
+                <label htmlFor="chars" className=' mb-4'>Choose the type of characters.</label>
+                <div className="flex gap-4 items-center">
+                    <input type="checkbox" onChange={() => setCheckedOne(!checkedOne)} />
+                    <label htmlFor="">Downcase letters</label>
+                </div>
 
-        <div className='flex flex-col w-1/4 my-4'>
-            <p className="text-center text-xl text-white mt-8 mb-4">Result:</p>
-            <input type="text" id="chars" className="text-center" value={finalPassword} maxLength={charNum} disabled={disableInput}/>
-        </div>
-    </>
+                <div className="flex gap-4 items-center">
+                    <input type="checkbox" onChange={() => setCheckedTwo(!checkedTwo)} />
+                    <label htmlFor="">Uppercase letters</label>
+                </div>
+                <div className="flex gap-4 items-center">
+                    <input type="checkbox" onChange={() => setCheckedThree(!checkedThree)} />
+                    <label htmlFor="">Special characters</label>
+                </div>
+
+                <div className="flex gap-4 items-center">
+                    <input type="checkbox" onChange={() => setCheckedFour(!checkedFour)} />
+                    <label htmlFor="">Numbers</label>
+                </div>
+            </div>
+            <button className="mt-4" onClick={() => setPassword(generatePassword())}>Generate</button>
+            <p className='text-red-600 mt-4'>{error}</p>
+
+            <div className='flex flex-col w-1/4 my-4'>
+                <p className="text-center text-xl text-white mt-8 mb-4">Result:</p>
+                <input type="text" id="chars" className="text-center" value={finalPassword} maxLength={charNum} disabled />
+            </div>
+        </>
     )
 }
