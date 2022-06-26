07638595dbd6da56d8dc6729ee09c1005df9d731
tomazcx <tomazcx06@gmail.com>
2022-06-26 11:37:25 -0300

    Adjusted labels

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -47,7 +47,7 @@ export const Complete = () => {
             <div className='flex flex-col'>
 
                 <label htmlFor="word" className='mb-4'>Insert words to shuffle</label>
-                <input type="text" className=" text-black p-4 py-2 rounded-md disabled:bg-gray-400" id="word" onChange={(e) => setWordInput(e.target.value)} />
+                <input type="text" className=" text-black p-4 py-2 rounded-md disabled:bg-gray-400" id="word" name="word" onChange={(e) => setWordInput(e.target.value)} />
 
                 <div className="flex gap-4 items-center justify-between">
                     <button onClick={() => clearWords()} className="flex-1 bg-red-300 text-black border border-red-300 hover:border-red-400 hover:bg-red-400 transition-colors p-4 py-2 rounded-md mt-4 disabled:bg-gray-600 disabled:border-gray-600">Clear</button>
@@ -72,8 +72,8 @@ export const Complete = () => {
 
 
                 <div className='flex flex-col mt-8 mb-3'>
-                    <p className="text-center text-xl mb-2">Result:</p>
-                    <input type="text" id="chars" className="text-black p-4 py-2 rounded-md text-center w-full disabled:bg-gray-400" value={password} disabled />
+                    <label htmlFor="password" className="text-center text-xl mb-2">Result:</label>
+                    <input type="text" name="password" id="password" className="text-black p-4 py-2 rounded-md text-center w-full disabled:bg-gray-400" value={password} disabled />
                 </div>
                 <button className=" bg-blue-300 px-8 py-1.5 rounded-xl border-2 border-blue-300 text-gray-900 w-full hover:bg-blue-400 hover:border-blue-400 transition-colors disabled:border-blue-100 disabled:bg-blue-100 mb-32 " disabled={wordsArray.length < 1} onClick={() => setPassword(generatePassword())}>Generate</button>
 
--- src/components/Shuffled.jsx
+++ src/components/Shuffled.jsx
@@ -80,8 +80,8 @@ export const Shuffled = () => {
             </div>
             
             <div className='flex flex-col mb-3'>
-                <p className="text-center text-xl mt-8 mb-4">Result:</p>
-                <input type="text" id="chars" className="text-black p-4 py-2 rounded-md text-center w-full disabled:bg-gray-400" value={finalPassword} maxLength={charNum} disabled />
+                <label htmlFor="password" className="text-center text-xl mt-8 mb-3">Result:</label>
+                <input type="text" name="password" id="password" className="text-black p-4 py-2 rounded-md text-center w-full disabled:bg-gray-400" value={finalPassword} maxLength={charNum} disabled />
             </div>
             <button className=" bg-blue-300 px-8 py-1.5 rounded-xl border-2 border-blue-300 text-gray-900 w-full hover:bg-blue-400 hover:border-blue-400 transition-colors" onClick={() => setPassword(generatePassword())}>Generate</button>
             <p className='text-center text-red-600 mt-4 mb-32'>{error}</p>
